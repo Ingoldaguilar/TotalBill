70ad98820d6afe922686713f1ff724f4e890c8f0
ingold <ingoldaguilar45@gmail.com>
2023-02-28 14:54:03 -0600

    Create the Shoppping class and setting up the menu function
    - create the menu function

---
+++ Shopping.cpp
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.cc to edit this template
+ */
+
+/* 
+ * File:   Shopping.cpp
+ * Author: Admin
+ * 
+ * Created on February 28, 2023, 2:30 PM
+ */
+
+#include "Shopping.h"
+
+Shopping::Shopping() {
+}
+
+Shopping::Shopping(const Shopping& orig) {
+}
+
+Shopping::~Shopping() {
+}
+
---
+++ Shopping.h
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.h to edit this template
+ */
+
+/* 
+ * File:   Shopping.h
+ * Author: Admin
+ *
+ * Created on February 28, 2023, 2:30 PM
+ */
+
+#ifndef SHOPPING_H
+#define SHOPPING_H
+
+class Shopping {
+public:
+    Shopping();
+    Shopping(const Shopping& orig);
+    virtual ~Shopping();
+private:
+
+};
+
+#endif /* SHOPPING_H */
+
