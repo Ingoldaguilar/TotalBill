35adf6bef941e3e2b834f5862598dbad33fcd9de
ingold <ingoldaguilar45@gmail.com>
2023-02-28 14:42:28 -0600

    First commit, setting up the project

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Admin
+ *
+ * Created on February 28, 2023, 2:24 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
